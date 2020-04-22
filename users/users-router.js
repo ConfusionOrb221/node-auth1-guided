21c58e6a4ab616e88528ce5498b6860d5784b09b
Nathan <nate1898@gmail.com>
2020-04-21 23:40:12 -0400

    Posting notes

--- users/users-router.js
+++ users/users-router.js
@@ -2,12 +2,16 @@ const router = require("express").Router();
 
 const Users = require("./users-model.js");
 
+const restricted = require('../auth/restricted-middleware');
+
 router.get("/", (req, res) => {
-  Users.find()
+
+    Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
+  
 });
 
 module.exports = router;
